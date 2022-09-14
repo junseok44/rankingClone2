3c57ac9f877067b8d2f5c93297710ca8418151d5
unknown <newtonjjang@naver.com>
2022-09-14 16:42:08 +0900

    html markup

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,24 @@ import React from "react";
 const App = () => {
   return (
     <div className="Home">
-      <div className="Container">hello</div>;
+      <div className="Container__row">
+        <div className="row">
+          <div className="row__rank pink">S</div>
+        </div>
+        <div className="row">
+          <div className="row__rank orange">A</div>
+        </div>
+        <div className="row">
+          <div className="row__rank lemon">B</div>
+        </div>
+        <div className="row">
+          <div className="row__rank purple">C</div>
+        </div>
+        <div className="row">
+          <div className="row__rank grey">D</div>
+        </div>
+      </div>
+      <div className="Container__image">df</div>
     </div>
   );
 };
